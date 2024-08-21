76e51493d8a1f4fd80274e36daf023ba392691cc
RAHUL26R <rahulroy.profile@gmail.com>
2024-08-21 15:21:05 +0530

    Commit 1: Client App Created and github action yml file is configured for CI

---
+++ Shopping/Shopping.Client/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shopping.Client
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageFile { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
